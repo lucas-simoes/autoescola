fd82d8443697db79b210acf89d6d630a4b27a47d
jvictorsk8er <jvictorsilva@outlook.com>
2017-11-23 18:56:43 -0200

    banco

--- autoescola.sql
+++ autoescola.sql
@@ -23,6 +23,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(10) NOT NULL,
+  `empresasId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nome`, `empresasId`) VALUES
+(5, 'A', 2),
+(7, 'B', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `cidades`
 --
 
@@ -30,7 +51,15 @@ CREATE TABLE IF NOT EXISTS `cidades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `cidades`
+--
+
+INSERT INTO `cidades` (`id`, `nome`) VALUES
+(1, 'Guanhães'),
+(2, 'São João Evangelista');
 
 -- --------------------------------------------------------
 
@@ -41,23 +70,30 @@ CREATE TABLE IF NOT EXISTS `cidades` (
 CREATE TABLE IF NOT EXISTS `clientes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
-  `cpfCnpj` int(14) NOT NULL,
+  `cpfCnpj` varchar(20) DEFAULT NULL,
   `nascimento` date DEFAULT NULL,
-  `fixo` int(11) DEFAULT NULL,
-  `celular` int(11) DEFAULT NULL,
-  `endereco` varchar(80) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `bairro` varchar(80) NOT NULL,
-  `estado` varchar(2) NOT NULL,
-  `cidadeId` int(11) NOT NULL,
-  `cep` int(8) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `endereco` varchar(80) DEFAULT NULL,
+  `bairro` varchar(40) DEFAULT NULL,
+  `uf` varchar(2) DEFAULT NULL,
+  `cidadeId` int(11) DEFAULT NULL,
+  `cep` varchar(10) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `empresasId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `cidadeId` (`cidadeId`),
   KEY `empresasId` (`empresasId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `cpfCnpj`, `nascimento`, `telefone`, `endereco`, `bairro`, `uf`, `cidadeId`, `cep`, `email`, `empresasId`) VALUES
+(1, 'João Victor Silva', '09518888620', '1988-01-08', '33988273723', 'Rua 7, 110', 'Bela Vista', 'MG', 1, '39740000', 'jvictorsilva@outlook.com', 1),
+(2, 'Natalia Caetano', '12345678920', '1989-09-18', '33988444214', 'Rua 7, 110', 'Bela Vista', 'MG', 2, '39740100', 'natalia@natalia.com', 2),
+(3, 'teste clientes', '095.485.485-55', '2017-10-26', '16519616516', 'fdafasdfafs', 'asdfa', 'MG', 1, '63165165', '', 2);
 
 -- --------------------------------------------------------
 
@@ -78,7 +114,49 @@ CREATE TABLE IF NOT EXISTS `empresas` (
   `uf` varchar(2) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`empresasId`),
   KEY `cidadeId` (`cidadeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+
+--
+-- Extraindo dados da tabela `empresas`
+--
+
+INSERT INTO `empresas` (`empresasId`, `nome`, `endereco`, `bairro`, `cidadeId`, `cep`, `telefone`, `cnpj`, `email`, `uf`) VALUES
+(1, 'CFC Califórnia Guanhães', 'Rua Alcindo Pereira', 'Centro', 2, '39740000', '3334214506', '12345678912345', 'teste@teste.com', 'MG'),
+(2, 'CFC Califórnia SJE', 'TGADFASFA', 'ASDFASFA', 1, '39740000', '33988273723', '98749541654984', 'TESTE@TESTE.COM', 'MG'),
+(3, 'CFC Califórnia Sabinopolis', 'Rua Alcindo Pereira', 'Centro', 2, '39740000', '(33) 3421-4506', '12345678901234', 'teste@teste.com', 'MG'),
+(4, 'CFC Califórnia Virginopolis', 'TGADFASFA', 'ASDFASFA', 1, '39740000', '33988273723', '65165165168519', 'TESTE@TESTE.COM', 'MG'),
+(5, 'CFC Califórnia Serro', 'Rua Alcindo Pereira', 'Centro', 2, '39740000', '(33) 3421-4506', '12345678901234', 'teste@teste.com', 'MG');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `itenscategoria`
+--
+
+CREATE TABLE IF NOT EXISTS `itenscategoria` (
+  `itensId` int(11) NOT NULL AUTO_INCREMENT,
+  `categoriasId` int(11) NOT NULL,
+  `produtosId` int(11) NOT NULL,
+  `quantidade` decimal(11,2) NOT NULL,
+  `valorUnitario` decimal(11,2) NOT NULL,
+  `valorDesconto` decimal(11,2) NOT NULL,
+  `valorTotalLiquido` decimal(11,2) NOT NULL,
+  `valorTotalPrazo` decimal(11,2) NOT NULL,
+  `modalidadesId` int(11) NOT NULL,
+  PRIMARY KEY (`itensId`),
+  KEY `modalidadesId` (`modalidadesId`),
+  KEY `categoriasId` (`categoriasId`),
+  KEY `produtosId` (`produtosId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
+
+--
+-- Extraindo dados da tabela `itenscategoria`
+--
+
+INSERT INTO `itenscategoria` (`itensId`, `categoriasId`, `produtosId`, `quantidade`, `valorUnitario`, `valorDesconto`, `valorTotalLiquido`, `valorTotalPrazo`, `modalidadesId`) VALUES
+(6, 5, 2, '10.00', '150.23', '0.00', '1502.30', '1502.30', 2),
+(8, 5, 3, '2.00', '60.00', '0.00', '120.00', '120.00', 2),
+(9, 7, 1, '1.00', '100.00', '0.00', '100.00', '253.30', 1);
 
 -- --------------------------------------------------------
 
@@ -100,7 +178,18 @@ CREATE TABLE IF NOT EXISTS `itensorcamento` (
   KEY `modalidadesId` (`modalidadesId`),
   KEY `orcamentosId` (`orcamentosId`),
   KEY `produtosId` (`produtosId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+
+--
+-- Extraindo dados da tabela `itensorcamento`
+--
+
+INSERT INTO `itensorcamento` (`itensId`, `orcamentosId`, `produtosId`, `quantidade`, `valorUnitario`, `valorDesconto`, `valorTotalLiquido`, `valorTotalPrazo`, `modalidadesId`) VALUES
+(1, 2, 1, '120.00', '100.00', '0.00', '12000.00', '12000.00', 2),
+(2, 2, 2, '1.00', '150.23', '0.00', '150.23', '150.23', 1),
+(3, 5, 3, '2.00', '60.00', '0.00', '160.00', '160.00', 2),
+(4, 6, 2, '1.00', '150.23', '0.00', '150.23', '250.23', 2),
+(5, 7, 1, '1.00', '100.00', '0.00', '100.00', '253.30', 2);
 
 -- --------------------------------------------------------
 
@@ -113,7 +202,15 @@ CREATE TABLE IF NOT EXISTS `modalidades` (
   `nome` varchar(20) COLLATE utf8_bin NOT NULL,
   `prazo` tinyint(1) NOT NULL,
   PRIMARY KEY (`modalidadesId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `modalidades`
+--
+
+INSERT INTO `modalidades` (`modalidadesId`, `nome`, `prazo`) VALUES
+(1, 'Cartão', 1),
+(2, 'A vista', 0);
 
 -- --------------------------------------------------------
 
@@ -138,7 +235,21 @@ CREATE TABLE IF NOT EXISTS `orcamentos` (
   KEY `clientesId` (`clientesId`),
   KEY `usuariosId` (`usuariosId`),
   KEY `empresasId` (`empresasId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=9 ;
+
+--
+-- Extraindo dados da tabela `orcamentos`
+--
+
+INSERT INTO `orcamentos` (`orcamentosId`, `data`, `clientesId`, `valorBruto`, `valorDesconto`, `valorLiquido`, `status`, `usuariosId`, `validade`, `valorPrazo`, `inclusao`, `empresasId`) VALUES
+(1, '2017-10-25', 1, '0.00', '0.00', '0.00', 1, 3, '2017-11-24', '0.00', '2017-10-25 02:00:00', 1),
+(2, '2017-11-06', 1, '0.00', '0.00', '0.00', 1, 3, '2017-12-06', '0.00', '2017-11-06 02:00:00', 1),
+(3, '2017-11-20', 1, '0.00', '0.00', '0.00', 1, 3, '2017-12-20', '0.00', '2017-11-20 02:00:00', 1),
+(4, '2017-11-20', 1, '0.00', '0.00', '0.00', 1, 3, '2017-12-20', '0.00', '2017-11-20 02:00:00', 1),
+(5, '2017-11-20', 1, '0.00', '0.00', '0.00', 1, 3, '2017-12-20', '0.00', '2017-11-20 02:00:00', 1),
+(6, '2017-11-20', 2, '0.00', '0.00', '0.00', 1, 4, '2017-12-20', '0.00', '2017-11-20 02:00:00', 1),
+(7, '2017-11-20', 3, '0.00', '0.00', '0.00', 1, 4, '2017-12-20', '0.00', '2017-11-20 02:00:00', 2),
+(8, '2017-11-20', 2, '0.00', '0.00', '0.00', 1, 3, '2017-12-20', '0.00', '2017-11-20 02:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -152,8 +263,18 @@ CREATE TABLE IF NOT EXISTS `produto_servico` (
   `valorAvista` float DEFAULT NULL,
   `valorAprazo` float DEFAULT NULL,
   `produtoAutoEscola` tinyint(1) NOT NULL,
+  `empresasId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `produto_servico`
+--
+
+INSERT INTO `produto_servico` (`id`, `descricao`, `valorAvista`, `valorAprazo`, `produtoAutoEscola`, `empresasId`) VALUES
+(1, 'Aula de Direção', 100, 253.3, 1, 1),
+(2, 'Aula de Legislação', 150.23, 250.23, 1, 2),
+(3, 'Teste serviço', 60, 80, 0, 2);
 
 -- --------------------------------------------------------
 
@@ -182,21 +303,31 @@ CREATE TABLE IF NOT EXISTS `titulos` (
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(150) NOT NULL,
-  `cpf` int(11) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
   `nascimento` date DEFAULT NULL,
-  `fixo` int(11) DEFAULT NULL,
-  `celular` int(11) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
   `endereco` varchar(80) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `bairro` varchar(2) NOT NULL,
+  `bairro` varchar(40) NOT NULL,
   `cidadeId` int(11) NOT NULL,
-  `cep` int(8) NOT NULL,
+  `cep` varchar(10) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `empresasId` int(11) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(120) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `admin` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `cidadeId` (`cidadeId`),
   KEY `empresasId` (`empresasId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `nascimento`, `telefone`, `endereco`, `bairro`, `cidadeId`, `cep`, `email`, `empresasId`, `login`, `senha`, `uf`, `admin`) VALUES
+(3, 'Administrador1', '12345678989', '2017-10-21', '33988273723', 'Rua Sete, 110 ', 'Bela Vista', 1, '39740000', 'contato@solucoesquefacilitam.com.br', 1, 'admin', '202cb962ac59075b964b07152d234b70', 'MG', 1),
+(4, 'João Victor Silva', '09218888620', '1988-01-08', '33988273723', 'Rua Sete, 110 ', 'Bela Vista', 2, '39740000', 'jvictorsilva@outlook.com', 2, 'jvictor', '202cb962ac59075b964b07152d234b70', 'MG', 0);
 
 --
 -- Restrições para as tabelas dumpadas
@@ -214,6 +345,14 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `empresas`
   ADD CONSTRAINT `empresas_ibfk_1` FOREIGN KEY (`cidadeId`) REFERENCES `cidades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `itenscategoria`
+--
+ALTER TABLE `itenscategoria`
+  ADD CONSTRAINT `itenscategoria_ibfk_1` FOREIGN KEY (`categoriasId`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `itenscategoria_ibfk_2` FOREIGN KEY (`produtosId`) REFERENCES `produto_servico` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `itenscategoria_ibfk_3` FOREIGN KEY (`modalidadesId`) REFERENCES `modalidades` (`modalidadesId`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Restrições para a tabela `itensorcamento`
