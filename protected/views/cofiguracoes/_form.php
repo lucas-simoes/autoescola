d9d9abaca69af228d7370eea6cdc635b7415690c
Lucas Simões <lucas.simoes@facilsistemas.com.br>
2018-04-09 15:57:59 -0300

    correção da visualização da logomarca na tela de configurações

--- protected/views/cofiguracoes/_form.php
+++ protected/views/cofiguracoes/_form.php
@@ -15,7 +15,7 @@
 
         <div class="box-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <?php
                         $logo = cofiguracoes::model()->findByAttributes(array('chave' => 'logo'));
                         if (!isset($logo)) {
@@ -23,8 +23,12 @@
                         }
                     ?>
                     <?php if ($logo->valor != '') : ?>
-                    <?php echo CHtml::image(Yii::app()->createAbsoluteUrl('/') . '/imgs/' . $logo->valor, '', array('class'=>'img-responsive img-sm', 'style'=>'width: 50px')); ?>
+                    <?php echo CHtml::image(Yii::app()->createAbsoluteUrl('/') . '/imgs/' . $logo->valor, '', array('class'=>'img-responsive', 'style'=>'width: 200px')); ?>
                     <?php endif; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">                    
                     <?php echo CHtml::label('Logomarca', 'cofiguracoes[logo]') ?>
                     <?php echo CHtml::fileField('cofiguracoes[logo]', $logo->valor, array('class' => 'form-control')); ?>
                 </div>
