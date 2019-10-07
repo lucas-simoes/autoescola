2acb7ad200763d2af533f74093c50ba71578564f
lucas-simoes <54647352+lucas-simoes@users.noreply.github.com>
2019-10-07 13:55:52 -0300

    Atualização

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,13 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-  /*'connectionString' => 'mysql:host=mysql.hostinger.com.br;dbname=u105903125_main',
-	'emulatePrepare' => true,
-	'username' => 'u105903125_main',
-	'password' => 'admin123@',
-	'charset' => 'utf8',*/
-    
-    'connectionString' => 'mysql:host=localhost;dbname=autoescola',
+  'connectionString' => 'mysql:host=localhost;dbname=autoescola',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
